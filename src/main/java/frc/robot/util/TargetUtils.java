68863f5b3022251a6885f58ab22180f3f1cbe2d1
Megh Patel <147993811+Megh-P@users.noreply.github.com>
2026-01-15 17:08:06 -0500

    Removed rotateBy (no more offset needed as 2024)

--- src/main/java/frc/robot/util/TargetUtils.java
+++ src/main/java/frc/robot/util/TargetUtils.java
@@ -55,7 +55,7 @@ public class TargetUtils {
     */
     public static Rotation2d getTargetHeadingToPoint(Pose2d robotPose, Translation2d point) {
         Rotation2d targetRotation = point.minus(robotPose.getTranslation()).getAngle();
-        return  targetRotation.rotateBy(Rotation2d.fromDegrees(180.0));
+        return  targetRotation;
     }
 
     public static double getDistanceToPoint(Pose2d pose, Translation2d point) {
